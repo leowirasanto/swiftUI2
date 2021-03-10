e042a19525936c7b948ee0611ee2d72dc7050a29
Leo Wirasanto <leo.laia@gojek.com>
2021-03-10 11:54:33 +0700

    initial commit

---
+++ SwiftUI2/SwiftUI2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI2
+//
+//  Created by Leo on 10/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
